037ba19948c57b9c2a9234fff9ab63aa3a68ffba
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2025-03-04 19:58:14 -0300

    add sistema de autenticação e autorização

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,4 +1,4 @@
-import CardWrapper, { Card } from "@/app/ui/dashboard/cards";
+import CardWrapper from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
@@ -8,7 +8,6 @@ import {
   RevenueChartSkeleton,
   CardsSkeleton,
 } from "@/app/ui/skeletons";
-import { fetchCardData, fetchLatestInvoices } from "@/app/lib/data";
 export default async function Page() {
   return (
     <main>
